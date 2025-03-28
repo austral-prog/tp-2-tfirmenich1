49a4c0a6bf174cd2ba510d76ba3856805e965652
tfirmenich1 <tfirmenich@mail.austral.edu.ar>
2025-03-28 12:01:50 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.title()} {last_name.title()}")
+
+ada()
+
